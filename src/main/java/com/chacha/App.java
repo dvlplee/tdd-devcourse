95062a0e259f947637a366f1afe3016fc6a71878
dvlplee <dvlplee16@gmail.com>
2025-07-31 14:34:20 +0900

    first commit

---
+++ src/main/java/com/chacha/App.java
@@ -0,0 +1,7 @@
+package com.chacha;
+
+public class App {
+    public void run() {
+        System.out.println("==계산기==");
+    }
+}
---
+++ src/test/java/com/chacha/AppTest.java
@@ -0,0 +1,12 @@
+package com.chacha;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    @DisplayName("테스트1")
+    void t1() {
+
+    }
+}
